068978d936635ea6ac953cb26db949325c82c36e
Leo Antelyes <leo.antelyes@viasat.com>
2017-11-06 16:56:25 -0800

    final push

--- src/app/profileview/profileview.component.ts
+++ src/app/profileview/profileview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user.service';
 import { ProfileService } from '../profile.service';
 
 @Component({
@@ -10,6 +11,7 @@ import { ProfileService } from '../profile.service';
 export class ProfileviewComponent implements OnInit {
 
   constructor(private profileService: ProfileService,
+              private userService: UserService,
               private route: ActivatedRoute) { }
 
   profile: Object = {};
@@ -23,10 +25,25 @@ export class ProfileviewComponent implements OnInit {
     });
   }
 
+  follow(username: string) {
+    this.userService.followUser(username).subscribe(response => {
+      this.profile['following'] = true;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  unfollow(username: string) {
+    this.userService.unfollowUser(username).subscribe(response => {
+      this.profile['following'] = false;
+    }, error => {
+      console.log(error);
+    })
+  }
+
   setProfile(username: string) {
     this.profileService.getProfile(username).subscribe(response => {
       this.profile = response.profile;
-      console.log(this.profile);
     }, error => {
       console.log(error);
     });
